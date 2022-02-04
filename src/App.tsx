8bb7fb1504862b9d35ebecb2ab078f2747b850e3
kostya-alch <kostafrompiter@gmail.com>
2022-02-04 23:24:18 +0300

    add loader

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import Header from './components/Header/Header';
 import Products from './components/Products/Products';
 import { store, persistor } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import Loader from './components/Loader/Loader';
 
 function App() {
   return (
     <>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
+        <PersistGate loading={<Loader />} persistor={persistor}>
           <Header />
           <Products />
         </PersistGate>
